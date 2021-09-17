e04e66fe1aa07487f96cc5c5da8dad062d8619cf
Sandeep Singh <s.sandeep79@ymail.com>
2021-09-17 15:53:25 +0530

    binary search: find first and last occurance of a number

--- src/Algos/sort&search/binarySearch.py
+++ src/Algos/sort&search/binarySearch.py
@@ -26,7 +26,7 @@ binarySearch(arr, x, low, high)
 def binarySearchIterative(arr: list, toSearch: int) -> int:
     if arr is None or len(arr) == 0: return -1
     
-    (low, high) = (0, len(arr))
+    (low, high) = (0, len(arr)-1)
     
     while low <= high:
         mid = (low+high)//2
@@ -47,7 +47,37 @@ def binarySearchRecursive(arr:list, toSearch:int, left:int, right:int) -> int:
     else: # if (toSearch > arr[mid])
         return binarySearchRecursive(arr, toSearch, mid+1, right)
 
+def binarySearchFirst(arr: list, n: int, x: int) -> int:
+    if arr is None or len(arr) == 0: return -1
 
+    (low, high) = (0, n-1)
+    result = -1
+
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == x:
+            result = mid
+            high = mid - 1
+        elif x < arr[mid]:high = mid - 1
+        else: low = mid+1
+    
+    return result
+
+def binarySearchLast(arr: list, n: int, x: int) -> int:
+    if arr is None or len(arr) == 0: return -1
+
+    (low, high) = (0, n-1)
+    result = -1
+
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == x:
+            result = mid
+            low = mid + 1
+        elif x < arr[mid]:high = mid - 1
+        else:low = mid+1
+    
+    return result
 
 array = [3, 4, 5, 6, 7, 8, 9]
 x = 4
@@ -55,11 +85,24 @@ x = 4
 resultIter = binarySearchIterative(array, x)
 resultRecursion = binarySearchRecursive(array, x, 0, len(array))
 
+array = [2, 4, 10, 10, 10, 18, 20]
+x = 10
+firstOccurance = binarySearchFirst(array, len(array), 10)
+lastOccurance = binarySearchLast(array, len(array), 10)
+
 if resultIter != -1:
     print("Element is present at index " + str(resultIter))
 else:
     print("Not found")
 if resultRecursion != -1:
     print("Element is present at index " + str(resultRecursion))
+else:
+    print("Not found")
+if firstOccurance != -1:
+    print("Element is present at index " + str(firstOccurance))
+else:
+    print("Not found")
+if lastOccurance != -1:
+    print("Element is present at index " + str(lastOccurance))
 else:
     print("Not found")
